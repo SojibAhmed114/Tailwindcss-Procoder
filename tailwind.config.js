42fa2bbcb4bc2e869c1c680ec22957177b29a677
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-07-17 18:27:26 +0600

    dark mood

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: ["*"],
   theme: {
+    screens: {
+      'sm': "576px",
+      'md': "768px"
+    },
+    fontFamily: {
+      'myFont':["Kanit",'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
